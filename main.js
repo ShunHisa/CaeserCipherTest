49967ba734ada965cd0f3bafdaeb5a707491d8a9
ShunHisa <shunsuke0000000@gmail.com>
2020-11-11 22:56:03 +0900

    スペース、端対応

--- main.js
+++ main.js
@@ -4,8 +4,17 @@ const upStrings = () => {
 
     let newcodes = Array.prototype.map.call(text, (t)=>{
         let code = t.charCodeAt(0);
-        console.log("code :",code);
-        return code-1;
+        console.log("originalcode :",code);
+        switch(code) {
+            case (32 || 12288): //スペースの時はそのまま 半角32 全角12288
+                return code;
+            case (97): //a->z
+                return 122;
+            case (65): //A->Z
+                return 90;
+            default:
+                return code-1;
+        }
     })
     let newtext = String.fromCharCode(...newcodes);
 
@@ -19,8 +28,16 @@ const downStrings = () => {
 
     let newcodes = Array.prototype.map.call(text, (t)=>{
         let code = t.charCodeAt(0);
-        console.log("code :",code);
-        return code+1;
+        switch(code) {
+            case (32 || 12288): //スペースの時はそのまま 半角32 全角12288
+                return code;
+            case (122): //z->a
+                return 97;
+            case (90): //Z->A
+                return 65;
+            default:
+                return code+1;
+        }
     })
     let newtext = String.fromCharCode(...newcodes);
 
